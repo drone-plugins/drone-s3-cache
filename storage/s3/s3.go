ced1e783eb098a2fed249effafd1b9a3be21b6d4
Don <don.j.olmstead@gmail.com>
2021-03-10 09:08:42 -0800

    Update to minio-go v7 API (#71)
    All the methods now need a context.Context passed in. Update to newer versions of all the APIs.

--- storage/s3/s3.go
+++ storage/s3/s3.go
@@ -1,14 +1,15 @@
 package s3
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"strings"
 
 	"github.com/drone/drone-cache-lib/storage"
 	"github.com/dustin/go-humanize"
-	"github.com/minio/minio-go/v6"
-	"github.com/minio/minio-go/v6/pkg/credentials"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/sirupsen/logrus"
 )
 
@@ -39,6 +40,7 @@ type Options struct {
 type s3Storage struct {
 	client *minio.Client
 	opts   *Options
+	ctx    context.Context
 }
 
 // New method creates an implementation of Storage with S3 as the backend.
@@ -57,7 +59,11 @@ func New(opts *Options) (storage.Storage, error) {
 			return nil, fmt.Errorf("could not connect to %s using IAM role: %w", opts.Endpoint, err)
 		}
 	}
-	client, err := minio.NewWithCredentials(opts.Endpoint, creds, opts.UseSSL, opts.Region)
+	client, err := minio.New(opts.Endpoint, &minio.Options{
+		Creds:  creds,
+		Secure: opts.UseSSL,
+		Region: opts.Region,
+	})
 
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to %s: %w", opts.Endpoint, err)
@@ -70,6 +76,7 @@ func New(opts *Options) (storage.Storage, error) {
 	return &s3Storage{
 		client: client,
 		opts:   opts,
+		ctx:    context.Background(),
 	}, nil
 }
 
@@ -85,14 +92,14 @@ func (s *s3Storage) Get(p string, dst io.Writer) error {
 		"key":    key,
 	}).Info("downloading file")
 
-	exists, err := s.client.BucketExists(bucket)
+	exists, err := s.client.BucketExists(s.ctx, bucket)
 	if err != nil {
 		return fmt.Errorf("error when accessing bucket %s: %w", bucket, err)
 	} else if !exists {
 		return fmt.Errorf("bucket %s does not exist", bucket)
 	}
 
-	object, err := s.client.GetObject(bucket, key, minio.GetObjectOptions{})
+	object, err := s.client.GetObject(s.ctx, bucket, key, minio.GetObjectOptions{})
 	if err != nil {
 		return fmt.Errorf("could not retrieve %s from %s: %w", bucket, key, err)
 	}
@@ -122,7 +129,7 @@ func (s *s3Storage) Put(p string, src io.Reader) error {
 		"key":    key,
 	}).Info("uploading file")
 
-	exists, err := s.client.BucketExists(bucket)
+	exists, err := s.client.BucketExists(s.ctx, bucket)
 	if err != nil {
 		return fmt.Errorf("error when accessing bucket %s: %w", bucket, err)
 	} else if !exists {
@@ -130,7 +137,7 @@ func (s *s3Storage) Put(p string, src io.Reader) error {
 	}
 
 	if !exists {
-		if err = s.client.MakeBucket(bucket, s.opts.Region); err != nil {
+		if err = s.client.MakeBucket(s.ctx, bucket, minio.MakeBucketOptions{Region: s.opts.Region}); err != nil {
 			return fmt.Errorf("could not create bucket %s: %w", bucket, err)
 		}
 		logrus.WithField("name", bucket).Info("bucket created")
@@ -138,7 +145,7 @@ func (s *s3Storage) Put(p string, src io.Reader) error {
 		logrus.WithField("name", bucket).Info("bucket found")
 	}
 
-	numBytes, err := s.client.PutObject(bucket, key, src, -1, minio.PutObjectOptions{ContentType: "application/tar"})
+	uploadInfo, err := s.client.PutObject(s.ctx, bucket, key, src, -1, minio.PutObjectOptions{ContentType: "application/tar"})
 	if err != nil {
 		return fmt.Errorf("could not put file in bucket %s at %s: %w", bucket, key, err)
 	}
@@ -146,7 +153,7 @@ func (s *s3Storage) Put(p string, src io.Reader) error {
 	logrus.WithFields(logrus.Fields{
 		"bucket": bucket,
 		"key":    key,
-		"size":   humanize.Bytes(uint64(numBytes)),
+		"size":   uploadInfo,
 	}).Info("file uploaded")
 	return nil
 }
@@ -163,23 +170,20 @@ func (s *s3Storage) List(p string) ([]storage.FileEntry, error) {
 		"key":    key,
 	}).Info("finding objects")
 
-	exists, err := s.client.BucketExists(bucket)
+	exists, err := s.client.BucketExists(s.ctx, bucket)
 	if err != nil {
 		return nil, fmt.Errorf("error when accessing bucket %s: %w", bucket, err)
 	} else if !exists {
 		return nil, fmt.Errorf("bucket %s does not exist", bucket)
 	}
 
-	// Create a done channel to control 'ListObjectsV2' go routine.
-	doneCh := make(chan struct{})
-
-	// Indicate to our routine to exit cleanly upon return.
-	defer close(doneCh)
-
 	var objects []storage.FileEntry
-	isRecursive := true
-	objectCh := s.client.ListObjectsV2(bucket, key, isRecursive, doneCh)
-	for object := range objectCh {
+	opts := minio.ListObjectsOptions{
+		Recursive: true,
+		Prefix:    key,
+	}
+
+	for object := range s.client.ListObjects(s.ctx, bucket, opts) {
 		if object.Err != nil {
 			return nil, fmt.Errorf("could not get file in bucket %s at %s: %w", bucket, object.Key, object.Err)
 		}
@@ -218,14 +222,14 @@ func (s *s3Storage) Delete(p string) error {
 		"key":    key,
 	}).Info("deleting object")
 
-	exists, err := s.client.BucketExists(bucket)
+	exists, err := s.client.BucketExists(s.ctx, bucket)
 	if err != nil {
 		return fmt.Errorf("error when accessing bucket %s: %w", bucket, err)
 	} else if !exists {
 		return fmt.Errorf("bucket %s does not exist", bucket)
 	}
 
-	err = s.client.RemoveObject(bucket, key)
+	err = s.client.RemoveObject(s.ctx, bucket, key, minio.RemoveObjectOptions{})
 	if err != nil {
 		return fmt.Errorf("could not delete file in %s at %s: %w", bucket, key, err)
 	}
